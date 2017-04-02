1f8b4329c0b9f71826c8f576d69d3bdd712b086b
IronGauntel <ssorokin2264@gmail.com>
2017-04-02 11:25:52 +0300

    tutorial project

---
+++ src/Calculate.java
@@ -0,0 +1,19 @@
+
+public class Calculate {
+
+    private int inum1;
+    private int inum2;
+
+    double dnum1;
+    double dnum2;
+
+    char act;
+
+    Calculate(int num1, int num2, char action)
+    {
+        inum1 = num1;
+        inum2 = num2;
+    }
+
+
+}
---
+++ src/tilt.java
@@ -0,0 +1,11 @@
+
+import java.io.*;
+
+public class tilt {
+    public static void main(String[] args) {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
